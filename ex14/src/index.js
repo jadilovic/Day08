e63db55df0631d0baf36cd82ede9f058878d8b43
jadilovic <adilovic79@yahoo.com>
2021-03-15 20:52:39 +0100

    adding exercises from 14 to 18

---
+++ ex14/src/index.js
@@ -0,0 +1,15 @@
+    // Only change code below this line
+
+    function convertStrToInt(str){
+        return parseInt(str);
+    }
+
+    convertStrToInt("55");
+
+    console.log(convertStrToInt("33"));
+    console.log(convertStrToInt("44"));
+    console.log(convertStrToInt("jefbezos"));
+
+    // Only change code above this line
+
+    module.exports = convertStrToInt;
